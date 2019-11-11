413b7975ba958da1f03e4bf7bf873ac18519105f
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-11 12:46:24 +0000

    Add findByUsername helper method

--- users/users-model.js
+++ users/users-model.js
@@ -1,9 +1,14 @@
 const db = require("../data/dbConfig.js");
 
 module.exports = {
-  add
+  add,
+  findByUsername
 };
 
 function add(user) {
   return db("users").insert(user);
+}
+
+function findByUsername(username) {
+    return db("users").where({ username });
 }
