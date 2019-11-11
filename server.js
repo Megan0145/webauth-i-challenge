1fe2c8049d5c4dd811f8e160bd0f9d7dd8d2394a
Megan Ennis <Megan@Megans-iMac-9.local>
2019-11-11 13:05:55 +0000

    Add restricted middleware to protect endpoint to get all users

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ server.post("/api/login", (req, res) => {
     });
 });
 
-server.get("/api/users", (req, res) => {
+server.get("/api/users", restricted, (req, res) => {
   users
     .find()
     .then(users => {
@@ -56,5 +56,21 @@ server.get("/api/users", (req, res) => {
       res.status(500).json({ message: "Could not get users: " + err.message });
     });
 });
+
+function restricted(req, res, next) {
+  const { username, password } = req.headers;
+  users
+    .findByUsername(username)
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        next();
+      } else {
+        res.status(401).json({ message: "You shall not pass: " });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: err.message });
+    });
+}
 
 module.exports = server;
